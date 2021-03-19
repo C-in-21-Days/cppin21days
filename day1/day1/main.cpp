b2ccbdcec493fe4b5cc9f7c9ab66993afac598cb
nikola.shopov <nikola.shopov@ozan.com>
2021-03-20 00:39:24 +0200

    I decided to use XCode as a tool, but we can change it if needed!

---
+++ day1/day1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  day1
+//
+//  Created by Nikola Shopov on 20.03.21.
+//
+
+#include <iostream>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    int x = 5;
+    int y = 7;
+    std::cout << std::endl;
+    std::cout << x + y << " " << x * y;
+    std::cout << std::endl;
+    return 0;
+}

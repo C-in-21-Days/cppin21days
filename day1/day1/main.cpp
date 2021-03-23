1a2c0c5924bcd93995019addf0bfe693739dca01
angelapavlova <59961313+angelapavlova@users.noreply.github.com>
2021-03-23 14:03:20 +0200

    Update main.cpp
    Done

--- day1/day1/main.cpp
+++ day1/day1/main.cpp
@@ -8,11 +8,12 @@
 #include <iostream>
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[]) { // argc - argument count; argv - argument vector; I found it on page 656, Day 17
     int x = 5;
     int y = 7;
     std::cout << std::endl;
-    std::cout << x + y << " " << x * y;
+    std::cout << x + y << " " << x * y<<std::endl; // adited by Angie on 23.03.21
+    printf("%d %d",x+y,x*y); // adited by Angie on 23.03.21 - wanted to check if that will work with the stdio.h library
     std::cout << std::endl;
     return 0;
 }

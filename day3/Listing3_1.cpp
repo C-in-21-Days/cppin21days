4637458c0900bfcf7ac212a2706d870bda4cc649
angelapavlova <59961313+angelapavlova@users.noreply.github.com>
2021-03-30 20:34:11 +0300

    Create Listing3_1.cpp

---
+++ day3/Listing3_1.cpp
@@ -0,0 +1,14 @@
+// Determining the Size of Variable Types on Your Computer
+
+#include<iostream>
+
+int main()
+{
+	using std::cout;
+	
+	cout<<"The size of an int is: \t\t"<<sizeof(int)<<"bytes.\n";
+	cout<<"The size of a short int is: \t\t"<<sizeof(short)<<"bytes.\n";
+	cout<<"The size of a long int is: \t\t"<<sizeof(long)<<"bytes.\n";
+	
+	return 0;
+}

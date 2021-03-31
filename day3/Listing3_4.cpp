2bafbc6ce07ff2e23a9521c31ad181d436f54f88
angelapavlova <59961313+angelapavlova@users.noreply.github.com>
2021-03-31 22:30:36 +0300

    Update Listing3_4.cpp

--- day3/Listing3_4.cpp
+++ day3/Listing3_4.cpp
@@ -1,2 +1,20 @@
 //  A Demonstration of Putting Too Large a Value in an unsigned short Integer
 
+#include <iostream>
+
+int main()
+{
+   using std::cout;
+   using  std::endl;
+   
+   unsigned short int a = 65535;
+   
+   a++;
+   cout<<a<<endl; //0
+   a++;
+   cout<<a<<endl; //1
+   a++;
+   cout<<a<<endl; //2
+
+    return 0;
+}

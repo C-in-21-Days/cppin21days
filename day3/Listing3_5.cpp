0b01f4e7fd6e0ec98134eedbd5d338df4b1ddacc
angelapavlova <59961313+angelapavlova@users.noreply.github.com>
2021-03-31 22:36:17 +0300

    Update Listing3_5.cpp

--- day3/Listing3_5.cpp
+++ day3/Listing3_5.cpp
@@ -7,15 +7,15 @@ int main()
    using std::cout;
    using std::endl;
    
-    short int a = 32767;
+    short int a = 32767; // if you change it to 32768, the first cout will show -32768
     
-   cout<<a<<endl;
+   cout<<a<<endl; //32767
    a++;
-   cout<<a<<endl;
+   cout<<a<<endl; //-32768
    a++;
-   cout<<a<<endl;
+   cout<<a<<endl; //-32767
    a++;
-   cout<<a<<endl;
+   cout<<a<<endl; //-32766
 
     return 0;
 }

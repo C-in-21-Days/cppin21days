cefb2b4cd42a47206f6f59c46e8e7ace7602a9db
angelapavlova <59961313+angelapavlova@users.noreply.github.com>
2021-06-10 12:13:31 +0300

    for loop
    listing 7.9

---
+++ day7/for_looping.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main()
+{
+    using namespace std;
+    
+    int counter;
+    for(counter=0;counter<5;counter++)
+        cout<<"Looping"<<endl;
+        
+    cout<<counter<<endl;
+    
+    return 0;
+}

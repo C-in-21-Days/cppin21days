41cfdc11fea302270c6431eabb9484dd3f39ae69
angelapavlova <59961313+angelapavlova@users.noreply.github.com>
2021-06-10 12:09:54 +0300

    Looping with while
    listing 7.8

---
+++ day7/looping_while.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+int main()
+{
+    using namespace std;
+    
+    int counter;
+    while(counter<5)
+    {
+        counter++;
+        cout<<"Looping! ";
+    }
+    
+    cout<<counter<<endl;
+    
+    return 0;
+}

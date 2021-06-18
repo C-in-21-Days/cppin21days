2b665195a5d1f7d1b4d1b336413319b27a8dbe3a
angelapavlova <59961313+angelapavlova@users.noreply.github.com>
2021-06-18 20:05:11 +0300

    Write a nested for loop that prints a 10x10 pattern of 0s

---
+++ day7/10x10.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    for(int i = 0;i<10;i++)
+    {
+        for (int j=0;j<10;j++)
+            cout<<"0";
+        
+    cout<<endl;
+    }
+    
+    return 0;
+}

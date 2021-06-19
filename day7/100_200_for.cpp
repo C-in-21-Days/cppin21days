cad0d186842f029fe888f3aaad0cee7019922290
angelapavlova <59961313+angelapavlova@users.noreply.github.com>
2021-06-19 09:33:38 +0300

    Write a for statement to count from 100 to 200 by twos

---
+++ day7/100_200_for.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for (int i=100;i<=200;i++)
+    {
+        if(i%2 == 0)
+            std::cout << i << std::endl;
+    }
+
+    return 0;
+}

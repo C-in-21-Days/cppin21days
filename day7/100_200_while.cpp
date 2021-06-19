733bf9999c5604cf7801c289f1f83b708e4ea948
angelapavlova <59961313+angelapavlova@users.noreply.github.com>
2021-06-19 09:39:47 +0300

    Write a while loop to count from 100 to 200 by twos.

---
+++ day7/100_200_while.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int count=100;
+    
+    while(count<=200)
+    {
+        std::cout << count << std::endl;
+        count+=2;
+        
+    }
+    
+
+    return 0;
+}

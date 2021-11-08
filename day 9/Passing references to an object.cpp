13b63d40b88812c2f059a95f30b389fa383eaa35
angelapavlova <59961313+angelapavlova@users.noreply.github.com>
2021-11-08 14:14:03 +0200

    Create Passing references to an object.cpp
    Because you know the object will never be null, it would be easier to work within the
    function if a reference were passed in, rather than a pointer.

---
+++ day 9/Passing references to an object.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+
+using namespace std;
+
+class SimpleCat
+{
+public:
+    SimpleCat();    //constructor
+    SimpleCat(SimpleCat&);      //copy constructor
+    ~SimpleCat();    //destructor
+
+    int GetAge() const {return itsAge;}
+    void SetAge(int age) {itsAge = age;}
+
+private:
+    int itsAge;
+};
+
+SimpleCat::SimpleCat()
+{
+    cout<<"Constructor"<<endl;
+    itsAge = 1;
+}
+SimpleCat::SimpleCat(SimpleCat&) {cout<<"Copy Constructor"<<endl;}
+SimpleCat::~SimpleCat() {cout<<"Destructor"<<endl;}
+
+const SimpleCat &FunctionTwo(const SimpleCat &theCat);
+
+int main()
+{
+    cout<<"Making a cat..."<<endl;
+    SimpleCat Frisky;
+    cout<<"Frisky is "<<Frisky.GetAge()<<" years old"<<endl;
+    Frisky.SetAge(5);
+    cout<<"Frisky is "<<Frisky.GetAge()<<" years old"<<endl;
+    cout<<"Calling FunctionTwo..."<<endl;
+    FunctionTwo(Frisky);
+    cout<<"Frisky is "<<Frisky.GetAge()<<" years old"<<endl;
+
+}
+
+const SimpleCat &FunctionTwo(const SimpleCat &theCat)
+{
+    cout<<"Function Two. Returning..."<<endl;
+    cout<<"Frisky is now "<<theCat.GetAge()<<" years old"<<endl;
+    return theCat;
+
+}

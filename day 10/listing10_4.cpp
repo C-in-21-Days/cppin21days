551e748a6cb83ed5ecd638e1847225c70311a057
angelapavlova <59961313+angelapavlova@users.noreply.github.com>
2022-01-09 20:57:51 +0200

    Create listing10_4.cpp
    Constructor initialisation

---
+++ day 10/listing10_4.cpp
@@ -0,0 +1,49 @@
+//------------------ Listing 10.3 -----------------//
+//
+// Created on 09.01 by Angie
+//
+//Constructor initialization
+
+#include <iostream>
+using namespace std;
+
+class Rectangle
+{
+public:
+    Rectangle();
+    Rectangle(int width, int length);
+    ~Rectangle(){};
+    int GetWidth(){return itsWidth;}
+    int GetLength(){return itsLength;}
+private:
+    int itsWidth;
+    int itsLength;
+};
+
+Rectangle::Rectangle():
+itsWidth(5),itsLength(20)
+{}
+
+Rectangle::Rectangle(int width, int length):
+itsWidth(width),itsLength(length)
+{}
+
+int main()
+{
+    Rectangle Rect1;
+    cout<<"Rect1 width: "<<Rect1.GetWidth()<<endl;
+    cout<<"Rect1 length: "<<Rect1.GetLength()<<endl;
+    
+    int aWidth, aLength;
+    cout<<"Enter a width:";
+    cin>>aWidth;
+    cout<<"Enter a length:";
+    cin>>aLength;
+    
+    Rectangle Rect2(aWidth,aLength);
+    
+    cout<<"Rect2 width: "<<Rect2.GetWidth()<<endl;
+    cout<<"Rect2 length: "<<Rect2.GetLength()<<endl;
+    
+    return 0;
+}
